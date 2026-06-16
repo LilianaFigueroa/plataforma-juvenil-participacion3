049b0e028492b591ba65a869b9032d08bbe8c00a
Liliana Isabel Figueroa Rosero <figueroa.lilibelus@gmail.com>
2026-06-16 00:31:56 +0000

    Actualización clase 29

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express =require ("express");
-const fs =require("fs");
-const path =require("path");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
 const PORT = 3000;
 
@@ -10,43 +11,44 @@ app.use(express.static(__dirname));
 const rutaCandidatos = path.join(__dirname, "data", "candidatos.json");
 
 function leerCandidatos() {
-    const data = fs.readFileSync(rutaCandidatos, "utf8");
-    return JSON.parse(data);
+  const data = fs.readFileSync(rutaCandidatos, "utf8");
+  return JSON.parse(data);
 }
 
 function guardarCandidatos(candidatos) {
-    fs.writeFileSync(rutaCandidatos, JSON.stringify(candidatos, null, 2));
+  fs.writeFileSync(rutaCandidatos, JSON.stringify(candidatos, null, 2));
 }
 
-app.get("/api/candidatos", function(req, res) {
-    const candidatos = leerCandidatos();
-    res.json(candidatos);
+app.get("/api/candidatos", function (req, res) {
+  const candidatos = leerCandidatos();
+  res.json(candidatos);
 });
 
-app.post("/api/candidatos", function(req, res) {
-    const nuevoCandidato = {
-        id: Date.now(),
-        nombre:re.body.nombre,
-        rol: req.body.rol,
-        propuesta: req.body.propuesta
-        estado: "perfil de práctica académica"
-    };
+app.post("/api/candidatos", function (req, res) {
+  const nuevoCandidato = {
+    id: Date.now(),
+    nombre: req.body.nombre,
+    rol: req.body.rol,
+    propuesta: req.body.propuesta,
+    estado: "Perfil de práctica académica"
+  };
 
-    if(!nuevoCandidato.nombre || !nuevoCandidato.rol || !nuevoCandidato.propuesta) {
-        return res.status(400).json({ 
-            mensaje: "Faltan campos requeridos" 
-        });
-    }
+  if (!nuevoCandidato.nombre || !nuevoCandidato.rol || !nuevoCandidato.propuesta) {
+    return res.status(400).json({
+      mensaje: "Faltan datos obligatorios"
+    });
+  }
 
-const candidatos = leerCandidatos();
-candidatos.push(nuevoCandidato);
-guardarCandidatos(candidatos);
+  const candidatos = leerCandidatos();
+  candidatos.push(nuevoCandidato);
+  guardarCandidatos(candidatos);
 
-res.status(201).json({ 
+  res.status(201).json({
     mensaje: "Perfil guardado correctamente",
     candidato: nuevoCandidato
+  });
 });
 
-app.listen(PORT, function() {
-    console.log("servidor funcionando en https://localhost:" + PORT);
+app.listen(PORT, function () {
+  console.log("Servidor funcionando en http://localhost:" + PORT);
 });
